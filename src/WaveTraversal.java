5f87a661b1e54e76513990f60c840d0b02b52a9f
hemantTsingh <hemantsingh1023@gmail.com>
2020-12-30 00:12:31 +0530

    New Wave Traversal

--- src/WaveTraversal.java
+++ src/WaveTraversal.java
@@ -7,22 +7,22 @@ public class WaveTraversal {
         System.out.println("Enter rows and columns "+" ");
         int m=scn.nextInt();
         int n=scn.nextInt();
-        int arr[][]=new int[m][n];
+        int arr[][]=new int[m][n];          //Entries of Matrix
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[0].length;j++){
                 arr[i][j]=scn.nextInt();
             }
         }
-        for(int j=0;j<arr[0].length;j++){
+        for(int j=0;j<arr[0].length;j++){           //Whenever wave is being forward, on even columns rows are increasing and on odd columns, rows are decreasing.
 
-            if(j==0){
+            if(j==0){                                   //Incrementing rows and hence printing
                 for(int i=0;i<arr.length;i++){
                     System.out.print(arr[i][j]+" ");
                 }
                 
             }
-            else{
-                for(int i=arr.length-1;i>=0;i--){
+            else{                                   
+                for(int i=arr.length-1;i>=0;i--){                           //decrementing rows and hence printing
                     System.out.print(arr[i][j]+" ");
                 }
 
