6be8e98e8f5878831cabfaeeea5414f58282880b
hemantTsingh <hemantsingh1023@gmail.com>
2020-12-29 23:04:26 +0530

    Matrix Multiplication

---
+++ src/proj_work.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class proj_work{
+    public static void main(String[] args){
+        System.out.println("Hemant Singh");
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter the digit");
+        int a= scn.nextInt();
+        System.out.println("Ab line badal jaegi");
+        int num[]={3,4,5,6,7,8,9,};
+        for(int i : num){
+            System.err.println(i);
+        }
+    }
+}
