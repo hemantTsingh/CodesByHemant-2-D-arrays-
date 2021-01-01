6de1ce82dce9f1ec1d7786e2bbd3e6dcf05a59a0
hemantTsingh <hemantsingh1023@gmail.com>
2021-01-02 01:01:59 +0530

    Switch Using Week Days !

---
+++ src/suntomonswitch.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class suntomonswitch {
+    public static void main(String[] args)throws Exception{
+
+      Scanner scn=new Scanner(System.in);
+      System.out.println("Daal bhai number");
+       int weekno=scn.nextInt();
+      
+      switch(weekno){
+          case 1:
+          if(weekno==1){
+              System.out.println("Sunday");
+              break;
+          }
+          case 2:
+          if(weekno==1){
+              System.out.println("Monday");
+              break;
+          }
+          case 3:
+          if(weekno==1){
+              System.out.println("Tuesday");
+              break;
+          }
+          case 4:
+          if(weekno==1){
+              System.out.println("Wednesday");
+              break;
+          }
+          case 5:
+          if(weekno==1){
+              System.out.println("Thursday");
+              break;
+          }
+          case 6:
+          if(weekno==1){
+              System.out.println("Friday");
+              break;
+          }
+          case 7:
+          if(weekno==7){
+              System.out.println("Saturday");
+              break;
+          }
+          default:
+          System.out.println("issey aage weeks nahi hote bhai");
+
+
+
+}
+
+
+
+
+
+
+    }
+    
+}
